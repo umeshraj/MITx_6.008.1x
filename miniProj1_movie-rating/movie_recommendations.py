7f4bbe57b13aad60f85a4053be7502caab41cd77
umesh rajashekar <umesh.rajashekar@gmail.com>
2016-10-10 19:43:52 -0400

    Part g/h: A fast implementation to compute entropies

--- miniProj1_movie-rating/movie_recommendations.py
+++ miniProj1_movie-rating/movie_recommendations.py
@@ -191,6 +191,7 @@ def infer_true_movie_ratings(num_observations=-1):
 
     for movie_id in movie_id_list:
         ratings = movie_data_helper.get_ratings(movie_id)
+        ratings = ratings[:num_observations]
         movie_posterior = compute_posterior(prior, likelihood, ratings)
         posteriors[movie_id, :] = movie_posterior
         MAP_ratings[movie_id] = np.argmax(movie_posterior)
@@ -201,7 +202,64 @@ def infer_true_movie_ratings(num_observations=-1):
 
     return posteriors, MAP_ratings
 
+def infer_true_movie_ratings_fast(movie_id_list, ratings):
+    """
+    For every movie, computes the posterior distribution and MAP estimate of
+    the movie's true/inherent rating given the movie's observed ratings.
 
+    Input
+    -----
+    - num_observations: integer that specifies how many available ratings to
+        use per movie (the default value of -1 indicates that all available
+        ratings will be used).
+
+    Output
+    ------
+    - posteriors: a 2D array consisting of the posterior distributions where
+        the number of rows is the number of movies, and the number of columns
+        is M, i.e., the number of possible ratings (remember ratings are
+        0, 1, ..., M-1); posteriors[i] gives a length M vector that is the
+        posterior distribution of the true/inherent rating of the i-th movie
+        given ratings for the i-th movie (where for each movie, the number of
+        observations used is precisely what is specified by the input variable
+        `num_observations`)
+    - MAP_ratings: a 1D array with length given by the number of movies;
+        MAP_ratings[i] gives the true/inherent rating with the highest
+        posterior probability in the distribution `posteriors[i]`
+    """
+
+    M = 11  # all of our ratings are between 0 and 10
+    prior = np.array([1.0 / M] * M)  # uniform distribution
+    likelihood = compute_movie_rating_likelihood(M)
+
+    # -------------------------------------------------------------------------
+    # YOUR CODE GOES HERE FOR PART (d)
+    #
+    # Your code should iterate through the movies. For each movie, your code
+    # should:
+    #   1. Get all the observed ratings for the movie. You can artificially
+    #      limit the number of available ratings used by truncating the ratings
+    #      vector according to num_observations.
+    #   2. Use the ratings you retrieved and the function compute_posterior to
+    #      obtain the posterior of the true/inherent rating of the movie
+    #      given the observed ratings
+    #   3. Find the rating for each movie that maximizes the posterior
+
+    # These are the output variables - it's your job to fill them.
+    num_movies = len(movie_id_list)
+    posteriors = np.zeros((1, M))
+    MAP_ratings = np.zeros(1)
+
+    movie_posterior = compute_posterior(prior, likelihood, ratings)
+    posteriors[0, :] = movie_posterior
+    MAP_ratings[0] = np.argmax(movie_posterior)
+    
+    #
+    # END OF YOUR CODE FOR PART (d)
+    # -------------------------------------------------------------------------
+
+    return posteriors, MAP_ratings
+    
 def compute_entropy(distribution):
     """
     Given a distribution, computes the Shannon entropy of the distribution in
@@ -230,6 +288,9 @@ def compute_entropy(distribution):
     # Be sure to:
     # - use log base 2
     # - enforce 0log0 = 0
+    distribution = distribution[distribution > 1e-8]
+    entropy = distribution * np.log2(distribution)
+    entropy = -1 * np.sum(entropy)
 
     #
     # END OF YOUR CODE FOR PART (f)
@@ -262,14 +323,39 @@ def compute_true_movie_rating_posterior_entropies(num_observations):
     # YOUR CODE GOES HERE FOR PART (g)
     #
     # Make use of the compute_entropy function you coded in part (f).
-
+        # get the list of all movie IDs to process
+    movie_id_list = movie_data_helper.get_movie_id_list()
+    num_movies = len(movie_id_list)
+    post, _ = infer_true_movie_ratings(num_observations)
+    posterior_entropies = np.zeros(num_movies)
+    for rowIdx in range(post.shape[0]):
+        posterior_entropies[rowIdx] = (compute_entropy(post[rowIdx,:]))
     #
     # END OF YOUR CODE FOR PART (g)
     # -------------------------------------------------------------------------
 
     return posterior_entropies
 
+    
+def compute_true_movie_rating_posterior_entropies_fast():
+    movie_id_list = movie_data_helper.get_movie_id_list()
+    num_movies = len(movie_id_list)
+    N = 200
+    finAns = np.zeros((num_movies, N))
+    for movie_id in movie_id_list:
+        print("Working on {0} of {1} movies".format(movie_id, num_movies))
+        allRatings = movie_data_helper.get_ratings(movie_id)
+        for nObs in range(1, N+1):
+            ratings = allRatings[:nObs]
+            posterior, _ = infer_true_movie_ratings_fast(movie_id_list, 
+                                                   ratings)
+            
+            finAns[movie_id, nObs-1] = compute_entropy(posterior)
 
+    # mean and plot
+    tmp = finAns.mean(axis=0)
+    plt.plot(tmp)
+            
 def main():
 
     # -------------------------------------------------------------------------
@@ -289,10 +375,6 @@ def main():
     print("Expected answer:")
     print(np.array([[0.91986917, 0.08013083]]))
 
-    # compute_movie_rating_likelihood(M=2)
-    
-    # movie ratings
-    movie_posteriors, movie_MAP_ratings = infer_true_movie_ratings()
     
     print("---")
     print("Entropy of fair coin flip")
@@ -328,6 +410,13 @@ def main():
     # easy for us graders to run your code. You may want to define multiple
     # functions for each of the parts of this problem, and call them here.
 
+    # compute_movie_rating_likelihood(M=2)
+    
+    # movie ratings
+    # movie_posteriors, movie_MAP_ratings = infer_true_movie_ratings()
+    #rateEnt= compute_true_movie_rating_posterior_entropies(num_observations=1)
+    compute_true_movie_rating_posterior_entropies_fast()
+    pass
     #
     # END OF YOUR CODE FOR TESTING
     # -------------------------------------------------------------------------
