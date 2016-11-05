7e773b633bf000c69c9fc8b8019b1ceded2610a8
umesh rajashekar <umesh.rajashekar@gmail.com>
2016-11-05 13:57:04 -0400

    replace get_obs with buildPhi

--- miniProj2_robot/robot/ViterbiOnly.py
+++ miniProj2_robot/robot/ViterbiOnly.py
@@ -21,13 +21,25 @@ def careful_log(x):
 def neglog(x):
     return -1 * careful_log(x)
 
-def get_obs(y_in):
-    phiOut = robot.Distribution()
+#def buildPhi(y_in):
+#    phiOut = robot.Distribution()
+#    for x in all_possible_hidden_states:
+#        y_poss = observation_model(x)
+#        if y_poss[y_in] > 0:
+#            phiOut[x] = y_poss[y_in]
+#    return phiOut
+
+def buildPhi(y):
+    phi_X = robot.Distribution()
     for x in all_possible_hidden_states:
-        y_poss = observation_model(x)
-        if y_poss[y_in] > 0:
-            phiOut[x] = y_poss[y_in]
-    return phiOut
+        if y is None:
+            phi_X[x] = 1
+        else:
+            yPoss = observation_model(x)
+            phi_X[x] = yPoss[y]
+    return phi_X
+
+
 
 def myDictMin(inDict):
     minVal = np.inf
@@ -69,8 +81,11 @@ all_possible_observed_states = robot.get_all_observed_states()
 prior_distribution = robot.initial_distribution()
 transition_model = robot.transition_model
 observation_model = robot.observation_model
-observations = [(2, 0), (2, 0), (3, 0), (4, 0), (4, 0),
-                (6, 0), (6, 1), (5, 0), (6, 0), (6, 2)]
+#observations = [(2, 0), (2, 0), (3, 0), (4, 0), (4, 0),
+#                (6, 0), (6, 1), (5, 0), (6, 0), (6, 2)]
+observations = [(1, 6), (4, 6), (4, 7), None, (5, 6),
+                (6, 5), (6, 6), None, (5, 5), (4, 4)]
+
 
 ## example with class coins
 #import classCoinsExample as cc
@@ -97,7 +112,7 @@ observations = [(2, 0), (2, 0), (3, 0), (4, 0), (4, 0),
 num_time_steps = len(observations)
 
 phi1 = robot.Distribution()
-obs1 = get_obs(observations[0])
+obs1 = buildPhi(observations[0])
 for x in obs1.keys():
     tmpProd= obs1[x] * prior_distribution[x]
     if tmpProd > 0:
@@ -126,18 +141,16 @@ for x2_state in all_possible_hidden_states:
 
 msgList = [m12]
 tBackList = [tBack12]
-
 startIdx = 2
 for idx in range(startIdx, num_time_steps):
     y = observations[idx-1]
-    phi2 = get_obs(y)
+    phi2 = buildPhi(y)
     prevMsg = msgList[idx-2]
     m23 = robot.Distribution()
     tBack23 = {}
     for x2_state in all_possible_hidden_states:
-        if idx == 7 and x2_state == (6, 0, 'stay'):
-            print('debug')
-
+#        if idx == 7 and x2_state == (6, 0, 'stay'):
+#            print('debug')
         x1_collect = {}
         for x1_state, x1_value in phi2.items():
             x2_x1_trans = transition_model(x1_state)
@@ -160,7 +173,7 @@ for idx in range(startIdx, num_time_steps):
 
 # %% just fake the tracke back for now
 finStates = [None] * num_time_steps
-phiLast = get_obs(observations[-1])
+phiLast = buildPhi(observations[-1])
 finhat, finState = mostLikely(phiLast, msgList[-1])
 finStates[-1] = finState
 #finStates[-1] = (6, 2, "down")
@@ -172,5 +185,6 @@ for idx in range(num_time_steps-1, 0, -1):
     finStates[idx-1] = prevState
     print("{0}: {1}".format(idx, curState))
 # first state update
-firstState = tBack[curState]
+firstState= tBack[curState]
+finStates[0] = firstState
 print("{0}: {1}".format(0, firstState))
